7a54897dd9fe01875c6f1147d46ee4831d5a1a18
arxip <arxipov1989@gmail.com>
2024-07-16 13:44:40 +0300

    1

---
+++ src/test/java/com/thompsonCode/fromNerzon/FromNerzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thompsonCode.fromNerzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromNerzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
